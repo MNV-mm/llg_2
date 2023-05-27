ee4273e818e539cb4c5a2f190d3d5a89402f2ba9
MNV-mm <87756291+MNV-mm@users.noreply.github.com>
2023-05-27 12:56:43 +0300

    Update llg2_lin_sw.py

--- llg2_lin_sw.py
+++ llg2_lin_sw.py
@@ -178,7 +178,7 @@ size = comm.Get_size()
 
 alpha1 = 0.0001 
 alpha2 = 10   #parameter alpha
-UU0 = 2*10/3 #Voltage (CGS)
+UU0 = 0*10/3 #Voltage (CGS)
 AA = 9.5*10**(-8) #4.3e-6 #2*10**(-8) #(erg/cm) - exchange constant
 
 # # Образец 27
