8b0be64260d1d9e638e36a1f72fe86c0802f77c8
MNV-mm <87756291+MNV-mm@users.noreply.github.com>
2023-05-29 12:05:20 +0300

    Update llg2_lin_sw.py

--- llg2_lin_sw.py
+++ llg2_lin_sw.py
@@ -740,7 +740,7 @@ while j <= 10:
     #file_txt.write(data)
     #file_txt.close()
     mwrite(route_0 + 'results/avg_table.txt', data, 'a', rank)
-    if i%2 == 0:
+    if i%4 == 0:
         m_file.write(m, T)
         #hd_v_file.write(phi, T)
         #diff_file.write(diffr, T)
