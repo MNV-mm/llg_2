b1d441e8c388ca734bf664bbddc86d3a4e96ea58
MNV-mm <87756291+MNV-mm@users.noreply.github.com>
2023-07-23 22:43:41 +0300

    Update electric_field_clip.py

--- electric_field_clip.py
+++ electric_field_clip.py
@@ -34,7 +34,7 @@ def pe_EF(a,b,c,Lx,Ly,Lz,angle):
     #z_max = 0.5
     p1 = Point(-Lx/2,-Ly/2,-Lz) #matrix : cos(a)  sin(a)
     p2 = Point(Lx/2,Ly/2,-1)    #         -sin(a) cos(a)
-    nx = 100
+    nx = 120
     ny = 3
     nz = 100
     mesh = BoxMesh(p1,p2,nx,ny,nz)
