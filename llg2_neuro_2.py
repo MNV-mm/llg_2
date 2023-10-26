fd2888ba13bc6dfea66da9ae6c421613fd5a3ab7
MNV-mm <87756291+MNV-mm@users.noreply.github.com>
2023-10-26 16:11:01 +0300

    Update llg2_neuro_2.py

--- llg2_neuro_2.py
+++ llg2_neuro_2.py
@@ -705,7 +705,7 @@ while j <= 10:
     #file_txt.write(data)
     #file_txt.close()
     mwrite(route_0 + 'results/avg_table.txt', data, 'a', rank)
-    if i%20 == 0:
+    if i%5 == 0:
         m_file.write(m, T)
         hd_v_file.write(phi, T)
         diff_file.write(diffr, T)
