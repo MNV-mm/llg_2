40ff093d6edcdc696bafabce5f1e2fa2ad9e184b
MNV-mm <87756291+MNV-mm@users.noreply.github.com>
2023-11-04 18:51:43 +0300

    Update llg2_neuro_2.py

--- llg2_neuro_2.py
+++ llg2_neuro_2.py
@@ -178,7 +178,7 @@ size = comm.Get_size()
 
 alpha1 = 2. # 1E-4 
 #alpha2 = 10   #parameter alpha
-UU0 = -4*10/3/100 #Voltage (CGS)
+UU0 = -2*10/3/100 #Voltage (CGS)
 AA = 9.5*10**(-8) #4.3e-6 #2*10**(-8) #(erg/cm) - exchange constant
 
 # # Образец 27
